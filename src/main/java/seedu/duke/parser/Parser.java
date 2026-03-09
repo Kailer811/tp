613ac9f560bc174318cb7da6bc5a5752f8eea380
Brian <brianleekahhoe15@gmail.com>
2026-03-10 03:24:01 +0800

    Added Basic Start-Interface and Parser for testing

---
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -0,0 +1,38 @@
+package seedu.duke.parser;
+
+import seedu.duke.command.Command;
+import seedu.duke.command.DoneCommand;
+import seedu.duke.command.RemoveCommand;
+import seedu.duke.command.ListCompletedCommand;
+import seedu.duke.command.ListIncompleteCommand;
+import seedu.duke.command.ListNeededCommand;
+
+public class Parser {
+
+    public static Command parseCommand(String input) {
+
+        if (input.equals("list completed")) {
+            return new ListCompletedCommand();
+        }
+
+        if (input.equals("list incomplete")) {
+            return new ListIncompleteCommand();
+        }
+
+        if (input.equals("list needed")) {
+            return new ListNeededCommand();
+        }
+
+        if (input.startsWith("done ")) {
+            String moduleCode = input.substring(5).trim();
+            return new DoneCommand(moduleCode);
+        }
+
+        if (input.startsWith("remove ")) {
+            String moduleCode = input.substring(7).trim();
+            return new RemoveCommand(moduleCode);
+        }
+
+        return null;
+    }
+}
